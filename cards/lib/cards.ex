4eba9847aa3106c213abde227676149c6e4b7940
Mrityunjay Kumar <kumarmrityunjay7034@gmail.com>
2022-04-20 19:04:56 +0530

    writing cases test and doc test

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -4,6 +4,9 @@ defmodule Cards do
     Provides methods for creating and handling deck for cards project
   """
 
+  @doc """
+    create_deck will return the strings represents each a paying card
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Diamonds", "Hearts"]
@@ -13,24 +16,59 @@ defmodule Cards do
     end
   end
 
+
+  @doc """
+    Returns shuffled deck of cards
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Return weather a card is exist in a deck or not
+
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> Cards.contains?(deck, "Ace of Diamonds")
+      true
+      iex> Cards.contains?(deck, "Ten of Spades")
+      false
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    This function will take two arguments. 1. deck 2. hand_size
+    based upon `hand_size` it will
+    return strings (where each string represents playing card)
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> {hand, rest_deck} = Cards.deal(deck, 3)
+      iex> hand
+      ["Ace of Spades", "Ace of Clubs", "Ace of Diamonds"]
+
+  """
   def deal(deck, hand_size) do
-    { cards, rest_cards } = Enum.split(deck, hand_size)
-    cards
+    Enum.split(deck, hand_size)
   end
 
+  @doc """
+    This function will save the deck locally.
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
 
+  @doc """
+    Loading deck from locally saved file.
+  """
   def load(filename) do
     { status, binary } = File.read(filename)
 
@@ -41,6 +79,9 @@ defmodule Cards do
   end
 
 
+  @doc """
+    Just execute and play!
+  """
   def create_hand(hand_size) do
     # deck = Cards.create_deck()
     # deck = Cards.shuffle(deck)
@@ -51,6 +92,4 @@ defmodule Cards do
     Cards.create_deck() |> Cards.shuffle() |> Cards.deal(hand_size)
 
   end
-
-
 end
--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -3,6 +3,23 @@ defmodule CardsTest do
   doctest Cards
 
   test "greets the world" do
-    assert Cards.hello() == :world
+    assert 1 + 1 == 2
   end
+
+  #  Writing second type of test which is  case
+  test "check number of cards in newly created deck" do
+    deck_length = length(Cards.create_deck())
+    assert deck_length == 20
+  end
+
+  # after shuffling of the deck is it equals to generated one ?
+  test "randomization of shuffled deck" do
+    deck = Cards.create_deck()
+    assert deck != Cards.shuffle(deck)
+    # another way to do exact same thing using refute keyword
+    # refute deck == Cards.shuffle(deck)
+  end
+
+
+
 end
